82eb47f964413a8d93cee815acb9de888928a48c
PrathamLalwani <prathamlalwani33@gmail.com>
2023-11-13 16:15:12 -0800

    added unit test

--- __tests__/unit/handlers/hello-from-lambda.test.js
+++ __tests__/unit/handlers/hello-from-lambda.test.js
@@ -1,21 +1,26 @@
 // Import all functions from hello-from-lambda.js
-// const lambda = require("../../../src/handlers/hello-from-lambda.js");
+const lambda = require("../../../src/handlers/send-email.js");
 
-// describe("Test for hello-from-lambda", () => {
-//   // This test invokes the hello-from-lambda Lambda function and compares the result
-//   it("Verifies successful response", async () => {
-//     // Invoke helloFromLambdaHandler
-//     const result = await lambda.helloFromLambdaHandler();
+describe("Test for hello-from-lambda", () => {
+  // This test invokes the hello-from-lambda Lambda function and compares the result
+  it("Verifies successful email response", async () => {
+    // Invoke helloFromLambdaHandler
+    const body = {
+      name: "Pratham Lalwani",
+      email: "prathamlalwani33@gmail.com",
+      message: "testing update",
+    };
+    const result = await lambda.handler(body);
 
-//     /*
-//             The expected result should match the return from your Lambda function.
-//             e.g.
-//                 If you change from `const message = 'Hello from Lambda!';` to `const message = 'Hello World!';` in hello-from-lambda.js,
-//                 you should change the following line to `const expectedResult = 'Hello World!';`
-//         */
-//     const expectedResult = "Hello from Local Lambda!";
+    /*
+            The expected result should match the return from your Lambda function.
+            e.g.
+                If you change from `const message = 'Hello from Lambda!';` to `const message = 'Hello World!';` in hello-from-lambda.js,
+                you should change the following line to `const expectedResult = 'Hello World!';`
+        */
+    const expectedResult = { statusCode: 200 };
 
-//     // Compare the result with the expected result
-//     expect(result).toEqual(expectedResult);
-//   });
-// });
+    // Compare the result with the expected result
+    expect(result.statusCode).toEqual(expectedResult.statusCode);
+  });
+});
