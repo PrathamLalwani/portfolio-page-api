ead22e72c95e9ef3817e60bacfb540da0ced6d6e
PrathamLalwani <prathamlalwani33@gmail.com>
2023-12-20 18:16:38 -0800

    added captcha

--- src/handlers/send-email.js
+++ src/handlers/send-email.js
@@ -3,7 +3,18 @@ const ses = new SESClient({ region: "ca-central-1" });
 
 export const handler = async (event) => {
   try {
-    const { name, email, message } = event;
+    const { name, email, message, token } = event;
+    const recaptchaResponse = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify?secret="6LftCTgpAAAAAM3oDPcGCw6O_UlcgpdWZsBnkMOd"&response=${token}`,
+    );
+    if (!recaptchaResponse.success) {
+      const response = {
+        statusCode: 500,
+
+        body: event,
+      };
+      return response;
+    }
     const command = new SendEmailCommand({
       Destination: {
         ToAddresses: ["prathamlalwani33@gmail.com"],
@@ -28,7 +39,7 @@ export const handler = async (event) => {
     return response;
   } catch (error) {
     const response = {
-      statusCode: 200,
+      statusCode: 500,
 
       body: event,
     };
